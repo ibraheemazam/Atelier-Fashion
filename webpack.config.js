5fd9b8f505860f3714bab930eddfe7670f413b89
Caroline Peake <peake.caroline@gmail.com>
2022-07-07 16:33:31 -0700

    initial webpack setup complete

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports={
+  mode: "development",
+  entry: path.join(__dirname, "/client/src/index.js"),
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "/client/dist")
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
