0eba3997600b9c1405ccd47a4c874b8bbed41027
xgmak94 <xgmak94@gmail.com>
2022-07-08 16:41:09 -0700

    Added folders

---
+++ client/src/components/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductDetail() {
+  return (
+    <div>
+      ADD PRODUCT DETAIL COMPONENTS HERE
+    </div>
+  );
+}
+
+export default ProductDetail;
---
+++ client/src/components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RatingsAndReviews() {
+  return (
+    <div>
+      ADD RATING AND REVIEWS COMPONENTS HERE
+    </div>
+  );
+}
+
+export default RatingsAndReviews;
