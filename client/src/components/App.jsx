13d68ebbce590bdc2733a2de58467524b851898b
xgmak94 <xgmak94@gmail.com>
2022-07-08 16:38:28 -0700

    Separated widgets into separate folders

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,19 @@
+/* eslint-disable import/extensions */
 import React from 'react';
+import ProductDetail from './ProductDetail/ProductDetail.jsx';
+import RelatedItems from './RelatedItems/RelatedItems.jsx';
+import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
+import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    return (
-      <div>Hello World</div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <ProductDetail />
+      <RelatedItems />
+      <RatingsAndReviews />
+      <QuestionsAndAnswers />
+    </div>
+  );
 }
 
 export default App;
