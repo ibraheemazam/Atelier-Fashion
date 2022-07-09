c62da5593a9a7f88197fc3f8b27c4db80f0d80c4
Ibraheem Azam <ibraheemazam@gmail.com>
2022-07-08 17:32:47 -0700

    add routes and controllers to the server to interact with HR API

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
+const router = require('./routes.js');
 
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use('/', router);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  // console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
