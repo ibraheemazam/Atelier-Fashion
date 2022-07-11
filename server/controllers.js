7b336e32ab4bea25f70e18baf49f9290d37760c1
xgmak94 <xgmak94@gmail.com>
2022-07-11 16:25:40 -0700

    Post a question completed

--- server/controllers.js
+++ server/controllers.js
@@ -2,11 +2,15 @@ const axios = require('axios');
 require('dotenv').config();
 
 const getProducts = (req, res) => {
-  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products', {
-    headers: {
-      Authorization: process.env.AUTH_TOKEN,
-    },
-  })
+  axios
+    .get(
+      'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products',
+      {
+        headers: {
+          Authorization: process.env.AUTH_TOKEN,
+        },
+      }
+    )
     .then((result) => {
       res.send(result.data);
     })
@@ -16,18 +20,24 @@ const getProducts = (req, res) => {
 };
 
 const getQuestions = (req, res) => {
-  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions', {
-    params: {
-      product_id: 40348,
-    },
-    headers: {
-      Authorization: process.env.AUTH_TOKEN,
-    },
-  }).then((result) => {
-    res.send(result.data);
-  }).catch((err) => {
-    res.status(400).send(err);
-  });
+  axios
+    .get(
+      'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions',
+      {
+        params: {
+          product_id: 40348,
+        },
+        headers: {
+          Authorization: process.env.AUTH_TOKEN,
+        },
+      }
+    )
+    .then((result) => {
+      res.send(result.data);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
 };
 
 const postQuestion = (req, res) => {
@@ -37,14 +47,28 @@ const postQuestion = (req, res) => {
     email: req.body.email,
     product_id: req.body.product_id,
   };
-  console.log(postBody);
 
-  res.sendStatus(501);
+  axios
+    .post(
+      'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions',
+      postBody,
+      {
+        headers: {
+          Authorization: process.env.AUTH_TOKEN,
+        },
+      },
+    )
+    .then((result) => {
+      console.log(result.data);
+      res.status(201).send(result.data);
+    })
+    .catch((err) => {
+      res.sendStatus(400);
+    });
+  // res.sendStatus(501);
 };
 
-const getReviews = (req, res) => {
-
-};
+const getReviews = (req, res) => {};
 
 exports.getProducts = getProducts;
 exports.getQuestions = getQuestions;
