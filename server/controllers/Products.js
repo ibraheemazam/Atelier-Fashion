5011c3047dab43513c7362e0d9777b3820ef552f
Caroline Peake <peake.caroline@gmail.com>
2022-07-13 13:57:33 -0700

    move controllers into separate Products folder

--- server/controllers/Products.js
+++ server/controllers/Products.js
@@ -4,10 +4,25 @@ require('dotenv').config();
 axios.defaults.headers.common.Authorization = process.env.AUTH_TOKEN;
 
 module.exports.getProducts = (req, res) => {
-  axios
-    .get(
-      'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products',
-    )
+  const productId = req.query.ID || null;
+  axios({
+    method: 'get',
+    url: productId ? `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${productId}` : 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products',
+  })
+    .then((result) => {
+      res.send(result.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send(err);
+    });
+};
+module.exports.getStyles = (req, res) => {
+  const productId = req.query.ID || null;
+  axios({
+    method: 'get',
+    url: productId ? `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${productId}/styles` : 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products',
+  })
     .then((result) => {
       res.send(result.data);
     })
@@ -15,3 +30,4 @@ module.exports.getProducts = (req, res) => {
       res.status(400).send(err);
     });
 };
+
