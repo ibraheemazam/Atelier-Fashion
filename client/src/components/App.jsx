3c842a329ad09c62e2354841e0f4e66c760e4677
Caroline Peake <peake.caroline@gmail.com>
2022-07-13 09:52:30 -0700

    add axios get product detail request in App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
 import ProductDetail from './ProductDetail/ProductDetail';
 import RelatedItems from './RelatedItems/RelatedItems';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers';
 import { GlobalContextProvider } from '../contexts/GlobalStore';
 
-function App() {
+function App(props) {
+  // would put this in general context
+  const [productData, setProductData] = useState({});
+
+  useEffect(() => {
+    const productId = window.location.pathname;
+    console.log(productId);
+    props.setProductID(productId);
+
+    axios
+      .get('/products', { params: { ID: `${productId}` } })
+      .then((results) => { setProductData(results.data); })
+      // .then using request url (maybe?) to setProductID
+      // .then((result) => {setData(result.data)}
+      // alert client when error
+      .catch((err) => { console.log(err); });
+  }, []);
+
+  //   // axios requests for each module
+
+
   return (
     <div>
-      <GlobalContextProvider>
-        <ProductDetail />
-        <RelatedItems />
-        <RatingsAndReviews />
-        <QuestionsAndAnswers />
-      </GlobalContextProvider>
+      <h1>Hello World</h1>
+      <div>
+        <GlobalContextProvider>
+          <ProductDetail />
+          <RelatedItems />
+          <RatingsAndReviews />
+          <QuestionsAndAnswers />
+        </GlobalContextProvider>
+      </div>
     </div>
   );
 }
