d044a9513257fc23836b7f861597e642ae7c76d1
Ibraheem Azam <ibraheemazam@gmail.com>
2022-07-13 18:01:31 -0700

    Renamed Helpfulness and fixed linter errors + declaring prop types

--- client/src/components/RatingsAndReviews/Helpfulness.jsx
+++ client/src/components/RatingsAndReviews/Helpfulness.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { format, parseISO } from 'date-fns';
 import axios from 'axios';
 
-function ReviewTile({ review }) {
+function Helpfulness({ review }) {
   const [helpfulness, setHelpfulness] = useState(review.helpfulness);
   // is it good to do this? Would it be better to pass down the getReviews func from
   // RatingsAndReviews? That would send a get request after put is sent. Rn there are
@@ -21,8 +20,7 @@ function ReviewTile({ review }) {
       }));
   };
 
-  const handleHelpfulClick = function handleHelpfulClick(event) {
-    console.log(event.target.value);
+  const handleHelpfulClick = function handleHelpfulClick() {
     const reviewID = review.review_id;
     if (!helpfulClicked.current) {
       putHelpfulRequest(reviewID)
@@ -37,23 +35,28 @@ function ReviewTile({ review }) {
   };
 
   return (
-    <Helpfulness>
+    <HelpfulnessDiv>
       <h5>Was this review helpful?</h5>
       <button type="button" onClick={handleHelpfulClick} value="yes">
         &nbsp;Yes&nbsp;
         {`(${helpfulness})`}
         &nbsp;
       </button>
-    </Helpfulness>
+    </HelpfulnessDiv>
   );
 }
 
-ReviewTile.propTypes = {
-  review: PropTypes.object.isRequired,
+Helpfulness.propTypes = {
+  review: PropTypes.shape({
+    body: PropTypes.string,
+    review_id: PropTypes.number,
+    summary: PropTypes.string,
+    helpfulness: PropTypes.number,
+  }).isRequired,
 };
 
-export default ReviewTile;
+export default Helpfulness;
 
-const Helpfulness = styled.div`
+const HelpfulnessDiv = styled.div`
   display: flex;
 `;
--- client/src/components/RatingsAndReviews/ReviewTile.jsx
+++ client/src/components/RatingsAndReviews/ReviewTile.jsx
@@ -1,43 +1,15 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { format, parseISO } from 'date-fns';
-import axios from 'axios';
-import Helpfullness from './Helpfullness';
+import Helpfulness from './Helpfulness';
 
 function ReviewTile({ review }) {
-  const [helpfulness, setHelpfulness] = useState(review.helpfulness);
-  // is it good to do this? Would it be better to pass down the getReviews func from
-  // RatingsAndReviews? That would send a get request after put is sent. Rn there are
-  // two sources of truth
-  const helpfulClicked = useRef(false);
   const starCount = [];
 
   for (let i = 0; i < review.rating; i += 1) {
     starCount.push(i);
   }
-
-  const handleHelpfulClick = function handleHelpfulClick(event) {
-    console.log(event.target.value);
-    const YorN = event.target.value;
-    const reviewID = review.review_id;
-
-    if (!helpfulClicked.current) {
-      axios.put(`/reviews/${reviewID}/helpful`)
-        .then((result) => {
-          console.log(`put to change helpful of review ${reviewID} was sent:\n`, result);
-          setHelpfulness(
-            YorN === 'yes'
-              ? helpfulness + 1
-              : helpfulness - 1,
-          );
-          helpfulClicked.current = true;
-        })
-        .catch((err) => {
-          console.log(`error for put to change helpful of review ${reviewID}:\n`, err);
-        });
-    }
-  };
 
   return (
     <Container>
@@ -82,13 +54,21 @@ function ReviewTile({ review }) {
         )
       }
       <br />
-      <Helpfullness review={review} />
+      <Helpfulness review={review} />
     </Container>
   );
 }
 
 ReviewTile.propTypes = {
-  review: PropTypes.object.isRequired,
+  review: PropTypes.shape({
+    body: PropTypes.string,
+    summary: PropTypes.string,
+    recommend: PropTypes.bool,
+    rating: PropTypes.number,
+    reviewer_name: PropTypes.string,
+    date: PropTypes.string,
+
+  }).isRequired,
 };
 
 export default ReviewTile;
@@ -112,53 +92,49 @@ const DateName = styled.div`
 const Response = styled.div`
   padding: 1em;
   background: lightgrey;
-`
+`;
 
 const Summary = styled.h3`
   display: flex;
   background: ;
 `;
 
-const Helpfulness = styled.div`
-  display: flex;
-`;
-
-const RevDiv = styled.div`
-  margin: 50px 0;
-  position: relative;
-  display: block;
-  color: red;
-  width: 0px;
-  height: 0px;
-  border-right: 100px solid transparent;
-  border-bottom: 70px solid red;
-  border-left: 100px solid transparent;
-  transform: rotate(35deg);
-  &:before {
-    border-bottom: 80px solid red;
-    border-left: 30px solid transparent;
-    border-right: 30px solid transparent;
-    position: absolute;
-    height: 0;
-    width: 0;
-    top: -45px;
-    left: -65px;
-    display: block;
-    content: '';
-    transform: rotate(-35deg);
-  };
-  &:after {
-    position: absolute;
-    display: block;
-    color: red;
-    top: 3px;
-    left: -105px;
-    width: 0px;
-    height: 0px;
-    border-right: 100px solid transparent;
-    border-bottom: 70px solid red;
-    border-left: 100px solid transparent;
-    transform: rotate(-70deg);
-    content: '';
-  };
-`;
+// const RevDiv = styled.div`
+//   margin: 50px 0;
+//   position: relative;
+//   display: block;
+//   color: red;
+//   width: 0px;
+//   height: 0px;
+//   border-right: 100px solid transparent;
+//   border-bottom: 70px solid red;
+//   border-left: 100px solid transparent;
+//   transform: rotate(35deg);
+//   &:before {
+//     border-bottom: 80px solid red;
+//     border-left: 30px solid transparent;
+//     border-right: 30px solid transparent;
+//     position: absolute;
+//     height: 0;
+//     width: 0;
+//     top: -45px;
+//     left: -65px;
+//     display: block;
+//     content: '';
+//     transform: rotate(-35deg);
+//   };
+//   &:after {
+//     position: absolute;
+//     display: block;
+//     color: red;
+//     top: 3px;
+//     left: -105px;
+//     width: 0px;
+//     height: 0px;
+//     border-right: 100px solid transparent;
+//     border-bottom: 70px solid red;
+//     border-left: 100px solid transparent;
+//     transform: rotate(-70deg);
+//     content: '';
+//   };
+// `;
