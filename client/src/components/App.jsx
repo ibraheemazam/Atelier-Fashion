87755d5d1550d5568059f3195cf6da1731ac7f20
Ibraheem Azam <ibraheemazam@gmail.com>
2022-07-16 12:26:52 -0700

    changes to add rev functionality. still wip

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,8 +10,8 @@ function App() {
   return (
     <StyledContainer>
       <GlobalContextProvider>
-        <ProductDetail />
-        <RelatedItems />
+        {/* <ProductDetail /> */}
+        {/* <RelatedItems /> */}
         <QuestionsAndAnswers />
         <RatingsAndReviews />
       </GlobalContextProvider>
--- client/src/components/RatingsAndReviews/ReviewList/AddRev.jsx
+++ client/src/components/RatingsAndReviews/ReviewList/AddRev.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useGlobalContext } from '../../../contexts/GlobalStore';
 
 function AddRev() {
+  const { productInfo } = useGlobalContext();
   const [addClicked, setAddClicked] = useState(false);
 
   const handleAddRev = function handleAddRev() {
@@ -21,34 +23,36 @@ function AddRev() {
       </button>
       {
         addClicked && (
-          <TestDiv>
-            <h2>Write a Review</h2>
-            <h3>About the Prod Name</h3>
-            <FormContainer onSubmit={handleSubmit}>
-              <label htmlFor="overall rating">
-                Overall rating*&nbsp;
-                <select>
-                  <option>1 star</option>
-                  <option>2 star</option>
-                  <option>3 star</option>
-                  <option>4 star</option>
-                  <option>5 star</option>
-                </select>
-              </label>
-              <label htmlFor="overall rating">
-                Do you recommend this product?*&nbsp;
-                <div>
-                  <label htmlFor="option one">Yes</label>
-                  <input type="radio" value="yes" id="choice1" />
-                </div>
-                <div>
-                  <label htmlFor="option two">No</label>
-                  <input type="radio" value="no" id="choice2" />
-                </div>
-              </label>
-              <button type="submit">Submit</button>
-            </FormContainer>
-          </TestDiv>
+          <AddRevBackground>
+            <TestDiv>
+              <h2>Write a Review</h2>
+              <h3>About the {productInfo.name}</h3>
+              <FormContainer onSubmit={handleSubmit}>
+                <label htmlFor="overall rating">
+                  Overall rating*&nbsp;
+                  <select>
+                    <option>1 star</option>
+                    <option>2 star</option>
+                    <option>3 star</option>
+                    <option>4 star</option>
+                    <option>5 star</option>
+                  </select>
+                </label>
+                <label htmlFor="overall rating">
+                  Do you recommend this product?*&nbsp;
+                  <div>
+                    <label htmlFor="option one">Yes</label>
+                    <input type="radio" value="yes" id="choice1" />
+                  </div>
+                  <div>
+                    <label htmlFor="option two">No</label>
+                    <input type="radio" value="no" id="choice2" />
+                  </div>
+                </label>
+                <button type="submit">Submit</button>
+              </FormContainer>
+            </TestDiv>
+          </AddRevBackground>
         )
       }
     </div>
@@ -57,13 +61,23 @@ function AddRev() {
 
 export default AddRev;
 
+const AddRevBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vw;
+  width: 100vw;
+  position: absolute;
+  top: 0; left: 0;
+  background: gold;
+`;
+
 const TestDiv = styled.div`
   padding: 1em;
-  background: gold;
-  height: 25em;
-  width: 25em;
-  position: relative;
-  top -470px; left: 80px;
+  background: pink;
+  height: 35%;
+  width: 50%;
+  border: 1px solid;
 `;
 
 const FormContainer = styled.form`
