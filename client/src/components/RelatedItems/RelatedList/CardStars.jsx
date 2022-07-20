85eb350b46b02bcae810f074ee29f2d0cb0efd23
Andy Chow <andychow94@gmail.com>
2022-07-19 20:13:24 -0700

    Completed adding review stars

--- client/src/components/RelatedItems/RelatedList/CardStars.jsx
+++ client/src/components/RelatedItems/RelatedList/CardStars.jsx
@@ -4,24 +4,31 @@ import PropTypes from 'prop-types';
 
 function CardStars({ reviewID }) {
   // console.log('Star data:', stars);
-  const totalStars = [];
+  const baseStars = [];
+  const filledStars = [];
   const reviews = reviewID.results;
   let average = 0;
   for (let i = 0; i < reviews.length; i += 1) {
     average += reviews[i].rating;
   }
   average /= reviews.length;
-  for (let i = 0; i < Math.round(average); i += 1) {
-    totalStars.push(<span className="star" key={i}>&#9733;</span>);
+  const partial = average * 20;
+  console.log('Partial star:', partial);
+  for (let i = 0; i < 5; i += 1) {
+    baseStars.push(<span className="empty-star" key={i}>&#9734;</span>);
   }
-  for (let i = 0; i < (5 - Math.round(average)); i += 1) {
-    totalStars.push(<span className="star" key={Math.round(average) + i}>&#9734;</span>);
+  for (let i = 0; i < 5; i += 1) {
+    filledStars.push(<span className="filled-star" key={i}>&#9733;</span>);
   }
+  console.log('Average:', average);
+  console.log('Partial:', partial);
+  console.log(baseStars);
+  console.log(filledStars);
 
   return (
     <Stars>
-      {/* <span className="star">&#9733;</span> */}
-      <span className="star">{totalStars}</span>
+      <BaseStar className="star">{baseStars}</BaseStar>
+      <FilledStar className="star" size={partial}>{filledStars}</FilledStar>
     </Stars>
   );
 }
@@ -35,8 +42,32 @@ CardStars.propTypes = {
 };
 
 const Stars = styled.div`
+  position: relative;
   margin-left: auto;
   margin-right: auto;
+  font-size: 25px;
+  color: gray;
+`;
+
+// const Overlay = styled.div`
+//   display: flex;
+//   flex-direction: row;
+// `;
+
+const BaseStar = styled.span`
+  position: relative;
+`;
+
+const FilledStar = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  display: flex;
+  width: ${(props) => props.size}%;
+  overflow:hidden;
+  flex-direction: row;
+  color: yellow;
+  font-size: bold;
 `;
 
 export default CardStars;
